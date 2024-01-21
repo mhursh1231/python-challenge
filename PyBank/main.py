40f1e09eb0e4894cc1dd89365492f0fac4900e7c
mhursh1231 <mhursh1231@gmail.com>
2024-01-21 14:25:38 -0500

    Push to repository

---
+++ PyBank/main.py
@@ -0,0 +1,84 @@
+#Pull in data from budget_data.csv
+import os
+import csv
+
+#Importing .csv file from local repo and outline of file path
+script_dir = os.path.dirname(os.path.abspath('budget_data.csv'))
+
+data_folder = os.path.join(script_dir, 'PyBank', 'Resources')
+
+file_path = os.path.join(data_folder, 'budget_data.csv')
+
+#Create the lists to store the data
+profit = []
+monthly_changes = []
+date = []
+
+#Initialize the variables
+count = 0
+total_profit = 0
+total_change_profits = 0
+initial_profit = 0
+monthly_change_profits = 0
+
+#Open the CSV file using the set path PyBankcsv
+with open(file_path, 'r') as csvfile:
+    #Create a CSV reader with dictionaries
+    csvreader = csv.reader(csvfile, delimiter=',')
+    #Read the header row first
+    csv_header = next(csvreader)
+
+    #Iterate through the rows in the CSV files
+    for row in csvreader:
+        
+        #Count number of months in dataset
+        count = count + 1
+        
+        final_profit = int(row[1])
+        
+        #Average change in profits on monthly basis
+        if count > 1:
+            monthly_change_profits = final_profit - initial_profit
+        
+        #Need month count for calculations of greatest increase/decrease in profits
+        date.append(row[0])
+
+        #Add the profit information and calculate total profits
+        profit.append(row[1])
+        total_profit = total_profit + int(row[1])
+        
+        #Store monthly changes
+        monthly_changes.append(monthly_change_profits)
+        total_change_profits = total_change_profits + monthly_change_profits
+        initial_profit = final_profit
+
+    #Calculate the average change in profits
+    average_change_profits = (total_change_profits)/(count - 1)
+
+    #Find the minimum and maximum in profits and respective dates 
+    greatest_increase_profits = max(monthly_changes)
+    greatest_decrease_profits = min(monthly_changes)
+
+    date_increase = date[monthly_changes.index(greatest_increase_profits)]
+    date_decrease = date[monthly_changes.index(greatest_decrease_profits)]
+
+    print("----------------------------------------------------------")
+    print("Financial Analysis")
+    print("----------------------------------------------------------")
+    print("Total Months: " + str(count))
+    print("Total Profits: " + "$" + str(total_profit))
+    print("Average Change: " + "$" + str(int(average_change_profits)))
+    print("Greatest Increase in Profits: " + str(date_increase) + " ($" + str(greatest_increase_profits) + ")")
+    print("Greatest Decrease in Profits: " + str(date_decrease) + " ($" + str(greatest_decrease_profits)+ ")")
+    print("----------------------------------------------------------")
+
+with open('financial_analysis.txt', 'w') as text:
+    text.write("----------------------------------------------------------\n")
+    text.write("Financial Analysis"+ "\n")
+    text.write("----------------------------------------------------------\n\n")
+    text.write("Total Months: " + str(count) + "\n")
+    text.write("Total Profits: " + "$" + str(total_profit) + "\n")
+    text.write("Average Change: " + "$" + str(int(average_change_profits)) + "\n")
+    text.write("Greatest Increase in Profits: " + str(date_increase) + " ($" + str(greatest_increase_profits) + ")\n")
+    text.write("Greatest Decrease in Profits: " + str(date_decrease) + " ($" + str(greatest_decrease_profits)+ ")\n")
+    text.write("----------------------------------------------------------\n")
---
+++ PyPoll/main.py
@@ -0,0 +1,69 @@
+#Pull in data from election_data.csv
+import os
+import csv
+
+#Importing .csv file from local repo and outline of file path
+script_dir = os.path.dirname(os.path.abspath('election_data.csv'))
+
+data_folder = os.path.join(script_dir, 'PyPoll', 'Resources')
+
+file_path = os.path.join(data_folder, 'election_data.csv')
+
+#Initialize variables & create the lists to store the data
+count = 0
+candidate_list = []
+vote_count = {}
+vote_percent = []
+
+#Open the CSV file using the set path PyPollcsv
+with open (file_path, newline="") as csvfile:
+    #Create a CSV reader with dictionaries
+    csvreader = csv.reader(csvfile, delimiter=',')
+    #Read the header row first
+    csv_header = next(csvreader)
+
+    #Iterate through the rows in the CSV files
+    for row in csvreader:
+        #Count total number of votes
+        count = count + 1 
+        #Set candidate names to candidate_list 
+        candidate_list.append(row[2])
+        #Create a set from candidate_list to get candidate_unique names
+    candidate_unique = set(candidate_list)
+
+    #calculate volte count and percentage for each candidate
+    for candidate in candidate_unique:   
+        # y is the total votes number per candidate
+        y = candidate_list.count(candidate)
+        vote_count[candidate] = y
+        # z is the total votes percent per candidate
+        z = (y/count)*100
+        vote_percent.append(z)
+    
+    winning_vote_count = max(vote_count.values())
+    winner = [candidate for candidate, votes in vote_count.items() if votes == winning_vote_count][0]
+
+    print("----------------------------------------------------------")
+    print("Election Results")
+    print("----------------------------------------------------------")
+    print("Total Votes: " + str(count))
+    print("----------------------------------------------------------")
+    for candidate in candidate_unique:
+        index = list(candidate_unique).index(candidate)
+        print(f"{candidate}: {vote_percent[index]}% ({vote_count[candidate]})")
+    print("----------------------------------------------------------")
+    print(f"Winner: {winner}")
+    print("----------------------------------------------------------")
+
+with open('election_results.txt', 'w') as text:
+    text.write("----------------------------------------------------------\n")
+    text.write("Financial Analysis"+ "\n")
+    text.write("----------------------------------------------------------\n")
+    text.write("Total Votes: " + str(count) + "\n")
+    text.write("----------------------------------------------------------\n")
+    for candidate in candidate_unique:
+        index = list(candidate_unique).index(candidate)
+        text.write(f"{candidate}: {vote_percent[index]}% ({vote_count[candidate]})\n")
+    text.write("----------------------------------------------------------\n")
+    text.write("The winner is: " + winner + "\n")
+    text.write("----------------------------------------------------------\n")
